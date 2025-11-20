e4396bdcdddc74bc0bdae5d49dbc5a7177da6869
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-11-20 16:39:38 +0600

    feat: implement Redux for state management; add middleware, store, and API setup

---
+++ src/redux/store.ts
@@ -0,0 +1,14 @@
+/** @format */
+
+import { configureStore } from "@reduxjs/toolkit";
+import baseApi from "./api/baseAPI";
+
+export const store = configureStore({
+  reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
+});
+
+export default store;
